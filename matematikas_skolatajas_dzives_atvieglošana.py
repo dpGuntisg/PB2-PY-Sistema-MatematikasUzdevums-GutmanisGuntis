7440b08b176c556dffd6f82f5616c94f7a29da05
dpGuntisg <106589314+dpGuntisg@users.noreply.github.com>
2022-06-03 09:11:54 +0300

    Update matematikas_skolatajas_dzives_atvieglošana.py

--- matematikas_skolatajas_dzives_atvieglošana.py
+++ matematikas_skolatajas_dzives_atvieglošana.py
@@ -1,1 +1,12 @@
 #Programma, kas palīdz matemātikas skolotājai
+#Programmu veidojis DP1-2 Audzēknis Guntis Gūtmanis
+
+
+
+def teorija():
+    print('a:b')
+    print(' - ')
+    print(' 2 ')
+teorija()
+
+# Pagaidām testēju kā izskatītos formulas pythonā
