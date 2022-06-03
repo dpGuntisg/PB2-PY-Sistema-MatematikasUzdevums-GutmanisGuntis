99996b9ff861f1ceb358d3680500d9e684a2a529
dpGuntisg <106589314+dpGuntisg@users.noreply.github.com>
2022-06-03 16:31:28 +0300

    Update matematikas_skolatajas_dzives_atvieglošana.py

--- matematikas_skolatajas_dzives_atvieglošana.py
+++ matematikas_skolatajas_dzives_atvieglošana.py
@@ -1,21 +1,36 @@
-#Programma, kas palīdz matemātikas skolotājai
-#Programmu veidojis DP1-2 Audzēknis Guntis Gūtmanis
+"""
+Programma, kas palīdz matemātikas skolotājai
+Programmu veidojis:
+ DP1-2 Audzēknis
+ Guntis Gūtmanis
+"""
 
 import shutil
 from time import sleep
+columns = shutil.get_terminal_size().columns
+
 
 def teorija():
-    columns = shutil.get_terminal_size().columns
+    
     print("Teorija".center(columns))
     sleep(1)
     print("Kombinatorikas pamatlikums".center(columns))
-    sleep(1)
-    print("Ja no dotās kopas kādu elementu A var izvēlēties n veidos, bet elementu B – m veidos, tad elementu pāri A un B var izvēlēties n * m veidos.")
+    sleep(2)
+    print("1.Saskaitīšanas likums:\n Ja no dotās kopas kādu elementu A var izvēlēties n veidos, bet elementu B – m veidos, tad A vai B var izvēlēties n + m veidos.")
     sleep(3)
-    print("Piemērs:\n Vienā šķīvī 5 dažādu šķirņu āboli, bet otrā – 4 dažādi bumbieri. Cik veidos var izvēlēties dažādu augļu pāri?  ")
-    sleep(10)
-    print("Risinājums:\n Katram izvēlētajam ābolam varam piekārtot 4 bumbierus no otrā šķīvja. Tātad kopā augļu pāri varam izvēlēties 5 * 4 = 20 veidos.")
-teorija()
+    print("2. Reizināšanas likums.\n Ja no dotās kopas kādu elementu A var izvēlēties n veidos, bet elementu B – m veidos, tad elementu pāri A un B var izvēlēties n *  m veidos.")
+
+def piemērs():
+    print("Kombinatorikas pamatlikuma piemērs\n\n".center(columns))
+    print("Piemērs")
+    print("Vienā šķīvī 5 dažādu šķirņu āboli, bet otrā – 4 dažādi bumbieri.")
+    print("   1) Cik veidos var izvēlēties vienu augli?\n   2) Cik veidos var izvēlēties dažādu augļu pāri?\n\n")
+
+#!!ATGĀDINĀJUMS IELIKT IESPĒJU LIETOTĀJAM IEVADĪT ATBILDI!!!
+    
+    print("Risinājums:\n   1) Jāizvēlas ābolu vai bumbieri, tātad 5+4=9 veidos.\n   2)Katram izvēlētajam ābolam varam piekārtot 4 bumbierus no otrā šķīvja. Tātad kopā augļu pāri varam izvēlēties 5⋅ 4 = 20 veidos.")
+
+piemērs()
 
 
 
