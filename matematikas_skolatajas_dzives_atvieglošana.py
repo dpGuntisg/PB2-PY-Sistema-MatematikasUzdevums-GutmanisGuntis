73fe4ff604119670c07a4b54adc1608b1c7d2635
dpGuntisg <106589314+dpGuntisg@users.noreply.github.com>
2022-06-03 09:38:41 +0300

    Update matematikas_skolatajas_dzives_atvieglošana.py

--- matematikas_skolatajas_dzives_atvieglošana.py
+++ matematikas_skolatajas_dzives_atvieglošana.py
@@ -1,12 +1,18 @@
 #Programma, kas palīdz matemātikas skolotājai
 #Programmu veidojis DP1-2 Audzēknis Guntis Gūtmanis
 
-
-
+import shutil
+from time import sleep
 def teorija():
-    print('a:b')
-    print(' - ')
-    print(' 2 ')
-teorija()
+    columns = shutil.get_terminal_size().columns
+    print("Teorija".center(columns))
+    sleep(1)
+    print("Kombinatorikas pamatlikums".center(columns))
+    sleep(1)
+    print("Ja no dotās kopas kādu elementu A var izvēlēties n veidos, bet elementu B – m veidos, tad elementu pāri A un B var izvēlēties n * m veidos.")
+    sleep(2)
+    print("Piemērs: Vienā šķīvī 5 dažādu šķirņu āboli, bet otrā – 4 dažādi bumbieri. Cik veidos var izvēlēties dažādu augļu pāri?  ")
+    sleep(3)
+    print("Risinājums. Katram izvēlētajam ābolam varam piekārtot 4 bumbierus no otrā šķīvja. Tātad kopā augļu pāri varam izvēlēties 5 * 4 = 20 veidos.")
 
-# Pagaidām testēju kā izskatītos formulas pythonā
+
