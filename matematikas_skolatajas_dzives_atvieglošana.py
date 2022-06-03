e6343bfbbed7b86519300736c877f2101b0a5a38
dpGuntisg <106589314+dpGuntisg@users.noreply.github.com>
2022-06-03 17:01:06 +0300

    Update matematikas_skolatajas_dzives_atvieglošana.py

--- matematikas_skolatajas_dzives_atvieglošana.py
+++ matematikas_skolatajas_dzives_atvieglošana.py
@@ -14,13 +14,13 @@ def teorija():
     
     print("Teorija".center(columns))
     sleep(1)
-    print("Kombinatorikas pamatlikums".center(columns))
+    print("Kombinatorikas pamatlikumi".center(columns))
     sleep(2)
     print("1.Saskaitīšanas likums:\n Ja no dotās kopas kādu elementu A var izvēlēties n veidos, bet elementu B – m veidos, tad A vai B var izvēlēties n + m veidos.")
     sleep(3)
     print("2. Reizināšanas likums.\n Ja no dotās kopas kādu elementu A var izvēlēties n veidos, bet elementu B – m veidos, tad elementu pāri A un B var izvēlēties n *  m veidos.")
 
-def piemērs():
+def piemers():
     print("Kombinatorikas pamatlikuma piemērs\n\n".center(columns))
     print("Piemērs")
     print("Vienā šķīvī 5 dažādu šķirņu āboli, bet otrā – 4 dažādi bumbieri.")
@@ -30,7 +30,13 @@ def piemērs():
     
     print("Risinājums:\n   1) Jāizvēlas ābolu vai bumbieri, tātad 5+4=9 veidos.\n   2)Katram izvēlētajam ābolam varam piekārtot 4 bumbierus no otrā šķīvja. Tātad kopā augļu pāri varam izvēlēties 5⋅ 4 = 20 veidos.")
 
-piemērs()
+def uzdevumi():
+    print("Pārbaudes darbs".center(columns))
+    print("Tēma Kombinatorikas pamatlikumi".center(columns))
+    print("Uzdevumi:\n")
+    print("1. Martai ir 3 šorti, 2 svārki un 4 bikses. Cik dažādu apģērbu komplektu var izveidot ar pelēko topu?\nAtbildes:")
+    
+uzdevumi()
 
 
 
