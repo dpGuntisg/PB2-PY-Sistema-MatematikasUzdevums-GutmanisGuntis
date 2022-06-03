e8b558e4e4ad43b63b7c12f1aa80786901f698e0
dpGuntisg <106589314+dpGuntisg@users.noreply.github.com>
2022-06-03 09:44:41 +0300

    Update matematikas_skolatajas_dzives_atvieglošana.py

--- matematikas_skolatajas_dzives_atvieglošana.py
+++ matematikas_skolatajas_dzives_atvieglošana.py
@@ -3,6 +3,7 @@
 
 import shutil
 from time import sleep
+
 def teorija():
     columns = shutil.get_terminal_size().columns
     print("Teorija".center(columns))
@@ -10,9 +11,11 @@ def teorija():
     print("Kombinatorikas pamatlikums".center(columns))
     sleep(1)
     print("Ja no dotās kopas kādu elementu A var izvēlēties n veidos, bet elementu B – m veidos, tad elementu pāri A un B var izvēlēties n * m veidos.")
-    sleep(2)
-    print("Piemērs: Vienā šķīvī 5 dažādu šķirņu āboli, bet otrā – 4 dažādi bumbieri. Cik veidos var izvēlēties dažādu augļu pāri?  ")
     sleep(3)
-    print("Risinājums. Katram izvēlētajam ābolam varam piekārtot 4 bumbierus no otrā šķīvja. Tātad kopā augļu pāri varam izvēlēties 5 * 4 = 20 veidos.")
+    print("Piemērs:\n Vienā šķīvī 5 dažādu šķirņu āboli, bet otrā – 4 dažādi bumbieri. Cik veidos var izvēlēties dažādu augļu pāri?  ")
+    sleep(10)
+    print("Risinājums:\n Katram izvēlētajam ābolam varam piekārtot 4 bumbierus no otrā šķīvja. Tātad kopā augļu pāri varam izvēlēties 5 * 4 = 20 veidos.")
+teorija()
+
 
 
